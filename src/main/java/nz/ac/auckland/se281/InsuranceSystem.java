7d0cede6bdad5a42f99f94bf735e9bfd65df2f8d
Wilson <wlia683@aucklanduni.ac.nz>
2023-03-23 00:30:04 +1300

    added additional checks to user inputs

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -8,6 +8,7 @@ public class InsuranceSystem {
 
 String userName;
 String age;
+int ageAsInteger;
 int rank = 1;
 ArrayList<Profile> database = new ArrayList<Profile>();
 Profile profile;
@@ -18,6 +19,7 @@ Profile profile;
   }
 
   public void printDatabase() {
+    // State the number of entries in the database
     if (database.size() == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0","s",".");
     } else if (database.size() == 1) {
@@ -27,15 +29,41 @@ Profile profile;
       String databaseSizeAsString = Integer.toString(databaseSize);
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage(databaseSizeAsString,"s",":");
     } 
+    // display all rank, name and age of all entries in the database
     for (Profile profile : database) {
       System.out.println(" " + profile.getRank() + ": " + profile.getUserName() + ", " + profile.getAge()); 
     }
     }
 
   public void createNewProfile(String userName, String age) {
-    Profile profile = new Profile(rank, userName, age);
+  //Check if user name is less than three letters
+    if (userName.length() < 3){
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+      return;
+    }
+    // Check if valid age
+    boolean isNumeric = age.chars().allMatch( Character::isDigit );
+    if (isNumeric == false){
+    MessageCli.INVALID_AGE.printMessage(age, userName);
+      return;}
+    ageAsInteger = Integer.parseInt(age);
+    if (ageAsInteger < 0){
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+      return;
+    }
+    //Check if user name is not unique
+    for (Profile profile : database) {
+      if (profile.getUserName().equals(userName)) {
+        MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+        return;
+      }
+    }
+    //Tidy up user name
+    String tidiedUserName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();
+     //Otherwise, if all good as profile to database
+    Profile profile = new Profile(rank, tidiedUserName, age);
     database.add(profile);
-    MessageCli.PROFILE_CREATED.printMessage(userName, age);
+    MessageCli.PROFILE_CREATED.printMessage(tidiedUserName, age);
     rank++;
   
   }
