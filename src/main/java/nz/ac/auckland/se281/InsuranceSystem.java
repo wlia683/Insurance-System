f3dd650b2aa6e1411da1f56093d6a2d2599c5d75
Wilson <wlia683@aucklanduni.ac.nz>
2023-03-23 20:17:56 +1300

    codestyle adjustments

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,63 +1,70 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
 
-String userName;
-String age;
-int ageAsInteger;
-int rank = 1;
-ArrayList<Profile> database = new ArrayList<Profile>();
-Profile currentProfile;
-  
-    public InsuranceSystem() {
+  String userName;
+  String age;
+  int ageAsInteger;
+  int rank = 1;
+  ArrayList<Profile> database = new ArrayList<Profile>();
+  Profile currentProfile;
+
+  public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-    
+
   }
 
   public void printDatabase() {
     // State the number of entries in the database
     if (database.size() == 0) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0","s",".");
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     } else if (database.size() == 1) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1","",":");
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
     } else {
       int databaseSize = database.size();
       String databaseSizeAsString = Integer.toString(databaseSize);
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(databaseSizeAsString,"s",":");
-    } 
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(databaseSizeAsString, "s", ":");
+    }
     // display all rank, name and age of all entries in the database
     for (Profile currentProfile : database) {
-      System.out.println(" " + currentProfile.getRank() + ": " + currentProfile.getUserName() + ", " + currentProfile.getAge()); 
+      System.out.println(
+          " "
+              + currentProfile.getRank()
+              + ": "
+              + currentProfile.getUserName()
+              + ", "
+              + currentProfile.getAge());
     }
-    }
+  }
 
   public void createNewProfile(String userName, String age) {
-    //Standardise user name entered
-    String tidiedUserName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();
+    // Standardise user name entered
+    String tidiedUserName =
+        userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
     userName = tidiedUserName;
 
-    //Check if user name is less than three letters
-    if (userName.length() < 3){
+    // Check if user name is less than three letters
+    if (userName.length() < 3) {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
       return;
     }
 
     // Check if valid age
-    boolean isNumeric = age.chars().allMatch( Character::isDigit );
-    if (isNumeric == false){
-    MessageCli.INVALID_AGE.printMessage(age, userName);
-      return;}
+    boolean isNumeric = age.chars().allMatch(Character::isDigit);
+    if (isNumeric == false) {
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+      return;
+    }
     ageAsInteger = Integer.parseInt(age);
-    if (ageAsInteger < 0){
+    if (ageAsInteger < 0) {
       MessageCli.INVALID_AGE.printMessage(age, userName);
       return;
     }
 
-    //Check if user name is not unique
+    // Check if user name is not unique
     for (Profile currentProfile : database) {
       if (currentProfile.getUserName().equals(userName)) {
         MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
@@ -65,21 +72,21 @@ Profile currentProfile;
       }
     }
 
-     //Otherwise, if all good add profile to database
+    // Otherwise, if all good add profile to database
     Profile currentProfile = new Profile(rank, userName, age);
     database.add(currentProfile);
     MessageCli.PROFILE_CREATED.printMessage(userName, age);
     rank++;
-  
   }
 
   public void loadProfile(String userName) {
 
-    //Standardise user name entered
-    String tidiedUserName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();
+    // Standardise user name entered
+    String tidiedUserName =
+        userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
     userName = tidiedUserName;
-    
-    //check through database for username entered, loads it and prints message if found
+
+    // check through database for username entered, loads it and prints message if found
     for (int i = 0; i < database.size(); i++) {
       if (database.get(i).getUserName().equals(userName)) {
         currentProfile = database.get(i);
@@ -89,19 +96,11 @@ Profile currentProfile;
         MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(userName);
       }
     }
-    //System.out.println("Currently loaded profile: " + currentProfile.getUserName() + ". Rank: " + currentProfile.getRank() + ". Age: " + currentProfile.getAge() + ".");
-
   }
 
-  public void unloadProfile() {
-  
-  }
+  public void unloadProfile() {}
 
-  public void deleteProfile(String userName) {
- 
-  }
+  public void deleteProfile(String userName) {}
 
-  public void createPolicy(PolicyType type, String[] options) {
-
-  }
+  public void createPolicy(PolicyType type, String[] options) {}
 }
