3e99f5c3e159f0c93ff117687bfb4cc75800d0a7
Wilson <wlia683@aucklanduni.ac.nz>
2023-04-18 15:24:17 +1200

    CarPolicy updated

--- src/main/java/nz/ac/auckland/se281/CarPolicy.java
+++ src/main/java/nz/ac/auckland/se281/CarPolicy.java
@@ -6,17 +6,39 @@ public class CarPolicy extends Policy {
 
   private String makeAndModel;
   private String registration;
-  private String mechanicalBreakdown;
+  private boolean mechanicalBreakdown;
+  private int premium;
+  private Profile currentProfile;
 
   public CarPolicy(int ID, PolicyType type, String[] options) {
     super(ID, type, options);
 
-    this.ID = ID;
-    this.type = type;
-    this.options = options;
     this.makeAndModel = options[1];
     this.registration = options[2];
-    this.mechanicalBreakdown = options[3];
+    if (options[3].equals("y")) {
+      this.mechanicalBreakdown = true;
+    } else if (options[3].equals("n")) {
+      this.mechanicalBreakdown = false;
+    }
+
+    for (Profile profile : InsuranceSystem.database) {
+      for (Policy policy : policyDatabase) {
+        if (policy.getID() == profile.getID()) {
+          currentProfile = profile;
+          break;
+        }
+      }
+    }
+
+    if (currentProfile.getAge() < 25) {
+      this.premium = (int) (this.getSumInsured() * 0.15);
+    } else {
+      this.premium = (int) (this.getSumInsured() * 0.1);
+    }
+  }
+
+  public int getPremium() {
+    return premium;
   }
 
   public String getMakeAndModel() {
@@ -27,7 +49,7 @@ public class CarPolicy extends Policy {
     return registration;
   }
 
-  public String getMechanicalBreakdown() {
+  public boolean getMechanicalBreakdown() {
     return mechanicalBreakdown;
   }
 }
